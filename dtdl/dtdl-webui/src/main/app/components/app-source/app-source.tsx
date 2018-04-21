16eb4f3a84f15667ac5734915ec4375904319725
Jesse Gallagher <jesse@secondfoundation.org>
2018-04-21 16:14:48 -0400

    Indent adjustment

--- dtdl/dtdl-webui/src/main/app/components/app-source/app-source.tsx
+++ dtdl/dtdl-webui/src/main/app/components/app-source/app-source.tsx
@@ -18,86 +18,86 @@ import { Component, Prop, State } from '@stencil/core';
 import { MatchResults } from '@stencil/router';
 
 @Component({
-  tag: 'app-source'
+    tag: 'app-source'
 })
 export class AppSource {
-  @Prop({ context: 'httpBase' }) private httpBase: string;
-  @Prop() match: MatchResults;
+    @Prop({ context: 'httpBase' }) private httpBase: string;
+    @Prop() match: MatchResults;
 
-  @State() private source: any;
-  @State() private issues: Array<any>;
+    @State() private source: any;
+    @State() private issues: Array<any>;
 
-  componentDidLoad() {
-    this.refreshSource();
-  }
-
-  refreshSource() {
-    try {
-      const url = new URL("$darwino-app/models/Source/" + encodeURIComponent(this.match.params.id), this.httpBase);
-      fetch(url.toString(), { credentials: 'include' })
-        .then(r => r.json())
-        .then(json => {
-          this.source = json.payload;
-
-          this.refreshIssues();
-        })
-        .catch(e => {
-            console.error("Error while connecting to server", e);
-        })
-    } catch(e) {
-        console.log(e);
+    componentDidLoad() {
+        this.refreshSource();
     }
-  }
 
-  refreshIssues() {
-    try {
-        const url = new URL("$darwino-app/issues/" + encodeURIComponent(this.match.params.id), this.httpBase);
-        fetch(url.toString(), { credentials: 'include' })
-          .then(r => r.json())
-          .then(json => {
-            this.issues = json.payload;
-          })
-          .catch(e => {
-              console.error("Error while connecting to server", e);
-          })
-      } catch(e) {
-          console.log(e);
-      }
-  }
+    refreshSource() {
+        try {
+            const url = new URL("$darwino-app/models/Source/" + encodeURIComponent(this.match.params.id), this.httpBase);
+            fetch(url.toString(), { credentials: 'include' })
+                .then(r => r.json())
+                .then(json => {
+                    this.source = json.payload;
 
-  renderIssues() {
-    if(this.issues == null) {
-      return [];
+                    this.refreshIssues();
+                })
+                .catch(e => {
+                    console.error("Error while connecting to server", e);
+                })
+        } catch (e) {
+            console.log(e);
+        }
     }
-    return this.issues.map(issue =>
-        <tr>
-            <td>{issue.title}</td>
-            <td><a href={issue.url} target="_blank">{issue.url}</a></td>
-        </tr>
-    );
-  }
 
-  render() {
-    if(this.source == null) {
-        return null;
+    refreshIssues() {
+        try {
+            const url = new URL("$darwino-app/issues/" + encodeURIComponent(this.match.params.id), this.httpBase);
+            fetch(url.toString(), { credentials: 'include' })
+                .then(r => r.json())
+                .then(json => {
+                    this.issues = json.payload;
+                })
+                .catch(e => {
+                    console.error("Error while connecting to server", e);
+                })
+        } catch (e) {
+            console.log(e);
+        }
     }
-    return (
-        <div>
-            <h1>{this.source._id}</h1>
-            <h3>{this.source.type}</h3>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Link</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.renderIssues()}
-                </tbody>
-            </table>
-        </div>
-    )
-  }
+    renderIssues() {
+        if (this.issues == null) {
+            return [];
+        }
+        return this.issues.map(issue =>
+            <tr>
+            <td>{ issue.title } < /td>
+            < td > <a href={ issue.url } target= "_blank" > { issue.url } < /a></td >
+            </tr>
+        );
+    }
+
+    render() {
+        if (this.source == null) {
+            return null;
+        }
+        return (
+            <div>
+            <h1>{ this.source._id } < /h1>
+            < h3 > { this.source.type } < /h3>
+
+            < table >
+            <thead>
+            <tr>
+            <th>Title < /th>
+            < th > Link < /th>
+            < /tr>
+            < /thead>
+            < tbody >
+            { this.renderIssues() }
+            < /tbody>
+            < /table>
+            < /div>
+        )
+    }
 }
